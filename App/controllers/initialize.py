1bd12c9c054c61edb543a9a67e9fc614368af75c
Terrence Murray <terrencemurray004@gmail.com>
2025-09-08 23:01:18 -0400

    Add more data to flask init

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -2,6 +2,8 @@ from .user import create_driver
 from App.database import db
 from .street import create_street
 from .user import create_resident
+from App.models import NotificationType
+from .notification import create_notification
 
 
 def initialize():
@@ -11,5 +13,44 @@ def initialize():
     streets_str = ['Randy Street', 'Author Street', 'Murray Drive', 'Charles Avenue']
     streets = [create_street(street) for street in streets_str]
 
-    create_driver('bob', 'bobpass', 'Terrence', 'Murray')
+    driver_1 = create_driver('bob', 'bobpass', 'Bob', 'Yi')
     create_resident('rick', 'rickpass', 'Rick', 'Smith', streets[0])
+    create_resident('amy', 'amypass', 'Amy', 'Persad', streets[1])
+
+    driver_1.schedule_stop(
+        street=streets[0],
+        date='2025-09-14'
+    )
+    create_notification(
+        street=streets[0],
+        notification_type=NotificationType.CONFIRMED,
+        message=f"A stop was successfully scheduled by '{driver_1.get_fullname()}' at street '{streets[0].name}' for '2025-09-14'.",
+    )
+
+    driver_1.schedule_stop(
+        street=streets[0],
+        date='2025-09-20'
+    )
+    create_notification(
+        street=streets[0],
+        notification_type=NotificationType.CONFIRMED,
+        message=f"A stop was successfully scheduled by '{driver_1.get_fullname()}' at street '{streets[0].name}' for '2025-09-20'.",
+    )
+
+    stop_1 = driver_1.schedule_stop(
+        street=streets[1],
+        date='2025-09-14'
+    )
+    create_notification(
+        street=streets[1],
+        notification_type=NotificationType.CONFIRMED,
+        message=f"A stop was successfully scheduled by '{driver_1.get_fullname()}' at street '{streets[1].name}' for '2025-09-14'.",
+    )
+
+    driver_1.mark_arrival(stop_1.id)
+    create_notification(
+        notification_type=NotificationType.ARRIVED,
+        message=f"'{driver_1.get_fullname()}' has arrived at your street.",
+        street = streets[0]
+    )
+
